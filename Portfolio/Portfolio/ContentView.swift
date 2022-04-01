d3e39b8c20307ae9f3f479dc5aef523176bacb99
Vicente <vppericone@gmail.com>
2022-03-31 20:41:17 -0700

    created project

---
+++ Portfolio/Portfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Portfolio
+//
+//  Created by JPL-ST-SPRING2021 on 3/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Portfolio/Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by JPL-ST-SPRING2021 on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
